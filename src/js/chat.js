ac7b05afd53ee1df2ddf4a5c8587848d301d94b3
Himayu18 <himayudhoke18@gmail.com>
2026-01-24 11:58:10 +0530

    responsive ui for desktop completed

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,10 +1,11 @@
 console.log("sidebar JS loaded");
 
-const side_bar_button = document.getElementById("sideBar")
+const side_bar_button = document.getElementById("sideBar");
 const sideBar = document.querySelector(".side-bar");
+const modelSelector = document.getElementById("model-selector");
 function openSideBar(){
     sideBar.classList.toggle("open");
-    console.log("sidebar JS loaded");
+    modelSelector.classList.add("adjust-model-selector")
 
 }
 side_bar_button.addEventListener("click",openSideBar)
@@ -12,6 +13,7 @@ side_bar_button.addEventListener("click",openSideBar)
 const close_side_bar = document.getElementById("close_sideBar")
 function closeSideBar(){
     sideBar.classList.toggle("open");
+    modelSelector.classList.remove("adjust-model-selector")
     console.log("sidebar JS loaded");
 
 }
